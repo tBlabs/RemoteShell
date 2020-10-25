c07d1fb0717d91c40db0ab681dbcc874cb03d40b
tB <wendro@wp.pl>
2020-10-25 22:55:23 +0100

    .env removed

--- src/startup.ts
+++ src/startup.ts
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 
-import * as dotenv from 'dotenv';
-dotenv.config(); // Loads variables from '.env' file to process.env
-
 import { IoC } from './IoC/IoC';
 import { Main } from './Main';
-
 
 (async () =>
 {
