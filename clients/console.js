25d6d1601aee0fb2d14b4d10afea19e4545314a2
tB <wendro@wp.pl>
2020-10-26 21:26:58 +0100

    client terminal history

--- clients/console.js
+++ clients/console.js
@@ -2,19 +2,42 @@ const input = document.querySelector("#InputTextbox");
 const runButton = document.querySelector("#RunButton");
 const output = document.querySelector("#OutputTextarea");
 
-const runRoutine = async () => 
+let history = ['pwd', 'ls'];
+let h = 1;
+
+input.value = history[h];
+
+function LoadHistory(c)
+{
+    h += c;
+    if (h <= 0) h = 0;
+    if (h >= history.length) h = history.length - 1;
+    input.value = history[h];
+}
+
+const RunRoutine = async () => 
 {
     output.textContent = "";
     const inputValue = input.value;
+    history.push(inputValue);
+    h = history.length - 1;
     const trackDirAsBase64 = btoa(inputValue);
     const response = await fetch('/shell64/' + trackDirAsBase64);
     const data = await response.text();
     output.textContent = data;
 }
 
-input.addEventListener('keypress', (e) =>
+input.onkeydown = (e) => 
 {
-    if (e.key === 'Enter') runRoutine();
-});
+    if (e.key === 'ArrowDown') LoadHistory(1);
+    if (e.key === 'ArrowUp') LoadHistory(-1);
+}
 
-runButton.addEventListener('click', runRoutine);
+input.onkeypress = (e) =>
+{
+    if (e.key === 'Enter') RunRoutine();
+}
+
+runButton.addEventListener('click', RunRoutine);
+
+input.focus();
